8fa1b5b2cc4ab621cd95614d4a0f7035611a5013
gplou <guillermop98@gmail.com>
2026-03-17 22:19:38 +0100

    fixes

--- lib/features/chat/data/chat_repository.dart
+++ lib/features/chat/data/chat_repository.dart
@@ -14,7 +14,7 @@ class ChatRepository {
     required String locale,
   }) async {
     if (!AiRateLimiter.instance.tryConsume()) {
-      throw RateLimitFailure(
+      throw const RateLimitFailure(
         'Límite alcanzado: máximo ${AiRateLimiter.maxPerMinute} usos por minuto. Espera un momento.',
       );
     }
